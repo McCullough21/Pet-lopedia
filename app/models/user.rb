9211801abadfdbecfe25314ed0a899feb962d3b1
github username <github email address>
2020-01-06 16:29:27 -0500

    scope method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :dogs
     has_many :breeds, through: :dogs
     has_secure_password
-    
+    scope :expert, => { where qualification: 'Veterinarian' || 'Breeder' || 'Vet-tech'}
+
 end
