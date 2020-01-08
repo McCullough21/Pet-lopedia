217b583b34a140aec43d481dc27ff34de40b9083
github username <github email address>
2020-01-08 10:39:54 -0500

    login validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
     has_many :breeds, through: :dogs
     has_secure_password
     scope :qualified, -> {where qualification: !nil}
-    
+    # skip_before_action :require_login, only: [:index]
+
+    def current_user
+        @user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
