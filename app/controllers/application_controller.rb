f2a25578451e8720fb3df018025691007f4167cc
github username <github email address>
2020-01-08 15:41:11 -0500

    login, params issue resolved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
 
 
     private
+    def current_user
+        @user ||= User.find_by(id: session[:user_id])
+    end
 
+    def logged_in?
+        !!current_user
+    end
     # def require_login
 #     return head(:forbidden) unless session.include? :user_id
 # end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    before_action :require_login
-    skip_before_action :require_login, only: [:home, :new]
+    # before_action :require_login
+    # skip_before_action :require_login, only: [:home, :new, :create]
     def home
         
     end
@@ -15,12 +15,20 @@ class UsersController < ApplicationController
 
     def login
         
-
+        @user = User.find_by(:username => params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:id] = @user.id
+            redirect_to user_path(@user.id)
+        else
+            redirect_to '/'
+        end
+        
     end
 
     def create
         
         @user = User.create(user_params)
+        session[:id] = @user.id
         redirect_to user_path(@user)
     end
 
@@ -42,8 +50,11 @@ class UsersController < ApplicationController
         params.require(:user).permit(:username, :password, :qualification)
     end
 
-    def require_login
-        return head(:forbidden) unless session.include? :user_id
-    end
+    # def require_login
+    #     unless logged_in?
+    #       flash[:error] = "You must be logged in to access this section"
+          
+    #     end
+    #   end
 
 end
