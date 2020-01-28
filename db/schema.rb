53367e239814c0fe66ff028726b8a0337756b5fc
github username <github email address>
2020-01-28 09:05:34 -0500

    Breed new for admin only

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_200424) do
+ActiveRecord::Schema.define(version: 2020_01_28_135606) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_200424) do
     t.string "qualification"
     t.string "email"
     t.string "uid"
+    t.boolean "admin"
   end
 
 end
